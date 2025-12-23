f32c3e267d03d1a55596c7042150ea9f8842d80a
Chronic-W-F <vandergriff4004@comcast.net>
2025-12-23 03:13:05 -0600

    Create iconMap.ts

---
+++ lib/iconMap.ts
@@ -0,0 +1,12 @@
+export const ICON_MAP: Record<string, string> = {
+  "Bud Rot": "/icons/bud-rot.png",
+  "CalMag Deficiency": "/icons/calmag.png",
+  "Clawing": "/icons/clawing.png",
+  "EC Spike": "/icons/ec-spike.png",
+  "Light Burn": "/icons/light-burn.png",
+  "Overwatering": "/icons/overwatering.png",
+  "Underwatering": "/icons/underwatering.png",
+  "Nitrogen Toxicity": "/icons/n-tox.png",
+  "Ph Lockout": "/icons/ph-lockout.png",
+  // add more over time
+};
