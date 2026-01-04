5269783ac4d4049fc65f0d298e841cf03f169279
Chronic-W-F <vandergriff4004@comcast.net>
2026-01-04 16:56:12 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const withPWA = require("next-pwa")({
   disable: process.env.NODE_ENV === "development",
 });
 
-module.exports = withPWA({
+const nextConfig = {
   reactStrictMode: true,
-});
+};
+
+module.exports = withPWA(nextConfig);
